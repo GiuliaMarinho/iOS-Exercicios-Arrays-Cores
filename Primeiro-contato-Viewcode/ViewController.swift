6f889e885fde4335321df7805ba3367077dbcdf3
GiuliaMarinho <giuliaketlin98@gmail.com>
2024-04-25 22:27:29 -0300

    first commit

---
+++ Primeiro-contato-Viewcode/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Primeiro-contato-Viewcode
+//
+//  Created by Giulia Marinho on 25/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        title = "Alteração de cores"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+}
